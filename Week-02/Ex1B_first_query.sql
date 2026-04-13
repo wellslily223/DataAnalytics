c8a90e8e560afe5623d4c2c01e0e2d15b82871d4
lily wells <lwells@my.yearupunited.org>
2026-04-13 11:31:42 -0400

    Added queries of actor and film tables

--- Week-02/Ex1B_first_query.sql
+++ Week-02/Ex1B_first_query.sql
@@ -1,3 +1,8 @@
 /*
 Sakila is a database used to collect information about categories and different types of data.
 /*
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 records
+
+SELECT * FROM film; -- Retrieved 1000 records
