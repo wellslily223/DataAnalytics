6a499036079dfde2697c3b4f99951f1f67327aee
wellslily223 <wellslily223@gmail.com>
2026-05-04 18:18:18 -0400

    1A

--- Week-05/Ex1A_hello.py
+++ Week-05/Ex1A_hello.py
@@ -1,1 +1,17 @@
 print('Hello world!')
+
+message = "Hello World!"
+print(message)
+
+# Hello world prints twice because the first message is a simplified version of the second code. 
+# Both of them lead to the same print, they are just formatted differently!
+
+# Displaying dollars and cents
+Dollars = 3
+Cents = .50
+print(Dollars + Cents)
+Cents = Cents + .25
+print(Dollars + Cents)
+d_str = '3 Dollars'
+c_str = '50 Cents'
+print(d_str + " " + c_str)
