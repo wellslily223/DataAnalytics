a8f1cad715c80be39e172de59034d4fb09288c1a
wellslily223 <wellslily223@gmail.com>
2026-05-04 21:33:22 -0400

    1B

---
+++ Week-05/Ex1B_variable_examples.py
@@ -0,0 +1,28 @@
+customer_id = 154 # I randomly created a 3-number value for this id. 
+customers_name = "Lucy Helming" # Like the words presented in the example, perhaps I should format it as 'first name' + 'last name'?
+customer_gender = "Female" # Rather average data - should stay as whatever the customer identifies as.
+customer_date_of_birth = "February 8th" # Perhaps I could write it down in a MM/DD/YY format for more specificity.
+drivers_license_number = T6719204 # I created a random set of numbers for this. Should always correspond with the drivers license numbers of the recorded customer. 
+auto_policy_number = 18249 # Similarly to the drivers license number, this is another randomly created set of numbers.
+
+first_name = "Lily"
+last_name = "Wells"
+city = "Philadelphia"
+state = "Pennsylvania"
+
+# The full list of words that cannot be used as variable names is;
+# False	def	if	raise
+# None	del	import	return
+# True	elif	in	try
+# and	else	is	while
+# as	except	lambda	with
+# assert	finally	nonlocal	yield
+# break	for	not
+# class	form	or
+# continue	global	pass
+
+# 'If' is used as a python keyword to add specific conditions to a statement.
+# 'And' is used to create additional modifiers to statements.
+# 'As' provides changes to a statement, relabeling it in the final output.
+# 'Import' makes specific modules available for use in the code.
+# 'Del' is used to remove unwanted objects from the code.
