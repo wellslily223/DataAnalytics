a209b8a36667189115b7b31fe939d46699fa2134
wellslily223 <wellslily223@gmail.com>
2026-05-05 10:14:57 -0400

    2A Lab 1

---
+++ Week-05/Ex2A_tips.py
@@ -0,0 +1,14 @@
+# Define known values
+food_cost = 79.25
+tax = 6.54
+tip = 12.00
+# Calculate the unknown
+total_due = food_cost + tax + tip
+# Display the results
+# print("The total due is " + str(total_due)) 
+
+print("Food cost is " + str(food_cost) + " and tax is " + str(tax))
+# print("Tip is " + str(tip))
+print("Total due is " + str(total_due))
+
+print("Tip is " + format(tip, ".2f"))
