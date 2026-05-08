d06fe60114c21bb7c7be36b08a8113f8c2b17fbc
wellslily223 <wellslily223@gmail.com>
2026-05-07 20:18:11 -0400

    3A Homework Part 2

---
+++ Week-05/Ex3A_string_cleaning.py
@@ -0,0 +1,39 @@
+name_1 = "PRIYA SHARMA"
+name_2 = "bob NGUYEN"
+name_3 = "LaTonya Williams"
+salary_1 = "$82,500"
+salary_2 = "$74,000"
+
+name_1 = name_1.lower()
+name_2 = name_2.lower()
+name_3 = name_3.lower()
+
+
+print(name_1)
+print(name_2)
+print(name_3)
+
+name_1 = name_1.title()
+name_2 = name_2.title()
+name_3 = name_3.title()
+
+
+print(name_1)
+print(name_2)
+print(name_3)
+
+salary_1 = salary_1.replace("$", "")
+salary_1 = salary_1.replace(",", "")
+salary_2 = salary_2.replace("$", "")
+salary_2 = salary_2.replace(",", "")
+
+print(salary_1)
+print(salary_2)
+
+print(type(salary_1))
+print(type(salary_2))
+
+salary_1 = int
+salary_1 = salary_1.replace(salary_1, ).int(salary_1)
+print(type(salary_1))
+
