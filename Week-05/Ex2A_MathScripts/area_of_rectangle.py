afa7cd1028e6232e3cd9fb609e4fd03bf09bf975
wellslily223 <wellslily223@gmail.com>
2026-05-11 01:50:02 -0400

    2A Lab 2 Area1

---
+++ Week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,10 @@
+import math
+
+side_a = int(input("Enter side_a: "))
+side_b = int(input("Enter side_b: "))
+number = (side_a * side_b)
+
+print(f' Side A is {side_a}')
+print(f' Side B is {side_b}')
+print(f' The area of the rectangle is:')
+print({number})
