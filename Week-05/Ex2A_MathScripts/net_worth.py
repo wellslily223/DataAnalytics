1e24cc11a9789079c1254e24dd365049125db221
wellslily223 <wellslily223@gmail.com>
2026-05-10 21:06:49 -0400

    2A Homework Part 2

--- Week-05/Ex2A_MathScripts/net_worth.py
+++ Week-05/Ex2A_MathScripts/net_worth.py
@@ -1,10 +1,11 @@
 import math
 
-assets = input("Enter your assets amount:")
-debt = input("Enter your debt amount:")
-net_worth = assets + debt
+assets = int(input("Enter your assets amount:"))
+debt = int(input("Enter your debt amount:"))
+net_worth = (assets - debt)
 
 print(f' Your total assets are {assets}')
 print(f' Your total debts are {debt}')
-print(f' Your net worth is {net_worth}')
+print(f' Your net worth is :')
+print(net_worth)
 
